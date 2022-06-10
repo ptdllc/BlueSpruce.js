ec3be0a5b96ad6353cf26c423fdc8048f969e854
Kai Lyons <kai@potabi.com>
2022-06-09 23:17:36 -0600

    Create base templates

---
+++ packages/create-bluespruce-app/template/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+  import "./app.css"
+</script>
+
+<main>
+  <img src={logo} alt="Svelte Logo" />
+  <h1>Hello world!</h1>
+
+  <Counter />
+  
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p class="w-2">
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+</main>
+
+<style></style>
