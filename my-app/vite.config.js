c2ed4e3b65b3dbf9efddb487a501954f2020fed2
Kai Lyons <kai@potabi.com>
2022-06-14 13:49:25 -0600

    Undo mistake

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,11 +1,11 @@
 import postcss from './postcss.config.js';
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  css: {
+  css:{
     postcss
   }
 })
